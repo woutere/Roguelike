935d6db219cb283f41a7e9aa8be5d43f4d045733
Wouter Eilers <c1042450@hik.be>
2020-10-18 21:24:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import tcod
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+            screen_width,
+            screen_height,
+            tileset=tileset,
+            title="The Golden monolith",
+            vsync=True
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
